f235fa3449aa8983efc03178d8771d425f4aa3f1
NathaelB <kirua48000@gmail.com>
2023-01-19 23:53:15 +0100

    :construction: create controller for `Auth`

---
+++ apps/Manager/Controllers/AuthController.ts
@@ -0,0 +1,35 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from "Domains/Users/Models/User";
+
+export default class AuthController {
+  public async login ({ auth, request, response }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    try {
+      const token = await auth.use('api').attempt(email, password)
+      response.send({
+        message: "Vous vous êtes bien connecté",
+        user: auth.user,
+        token: token
+      })
+    } catch {
+      return response.badRequest('Invalid credentials')
+    }
+  }
+
+  public async logout ({ auth }: HttpContextContract) {
+    await auth.use('api').revoke()
+    return {
+      revoked: true
+    }
+  }
+
+  public async me ({ auth }: HttpContextContract) {
+    const user = auth.user as User
+    user.load('permissions')
+    user.load('roles')
+
+    return user
+  }
+}
