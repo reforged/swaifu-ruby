fa0f79a605023400fb7851b4d070a71d9ecbbba2
NathaelB <kirua48000@gmail.com>
2023-01-20 09:34:01 +0100

    :construction: start data-structure for `Question`

---
+++ domains/Questions/Models/Question.ts
@@ -0,0 +1,50 @@
+import {
+  BaseModel,
+  beforeCreate,
+  BelongsTo,
+  belongsTo,
+  column,
+  ManyToMany,
+  manyToMany
+} from '@ioc:Adonis/Lucid/Orm'
+import {DateTime} from "luxon";
+import {randomUUID} from "crypto";
+import {slugify} from "@ioc:Adonis/Addons/LucidSlugify";
+import Etiquette from "Domains/Etiquettes/Models/Etiquette";
+import User from "Domains/Users/Models/User";
+
+export default class Question extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public label: string
+
+  @column()
+  @slugify({
+    strategy: 'dbIncrement',
+    fields: ['label'],
+    allowUpdates: true
+  })
+  public slug: string
+
+  @column()
+  public enonce: string
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @manyToMany(() => Etiquette)
+  public etiquettes: ManyToMany<typeof Etiquette>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static async generateUuid (model: Question) {
+    model.id = randomUUID()
+  }
+}
