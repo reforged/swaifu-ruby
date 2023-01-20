6a90c63893a2a0f79fb5611b073b494fbcc26a6b
NathaelB <kirua48000@gmail.com>
2023-01-20 09:28:59 +0100

    :construction: create validator

---
+++ apps/Manager/Validators/EtiquetteValidator.ts
@@ -0,0 +1,26 @@
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export class StoreValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    label: schema.string({ trim: true }),
+    description: schema.string({ trim: true }),
+    color: schema.string({ trim: true }),
+  })
+
+  public messages: CustomMessages = {}
+}
+
+export class UpdateValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    label: schema.string.optional({ trim: true }),
+    description: schema.string.optional({ trim: true }),
+    color: schema.string.optional({ trim: true }),
+  })
+
+  public messages: CustomMessages = {}
+}
